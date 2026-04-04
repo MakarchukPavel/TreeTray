f8be6036f8543fb800db0ce9a96ba98f735fc9de
P.Makarchuk@creatio.com <P.Makarchuk@creatio.com>
2026-04-04 12:58:46 +0300

    Fix tray popup hover selection and bump patch version

--- src/TreeTray/Views/TrayPopupWindow.cs
+++ src/TreeTray/Views/TrayPopupWindow.cs
@@ -1,9 +1,27 @@
 #region Class: TrayPopupWindow
 
+using Avalonia.VisualTree;
+
 namespace TreeTray.Views;
 
 public sealed class TrayPopupWindow : Window
 {
+	#region Struct: NativePoint
+
+	[StructLayout(LayoutKind.Sequential)]
+	private struct NativePoint
+	{
+		#region Fields: Public
+
+		public int X;
+
+		public int Y;
+
+		#endregion
+	}
+
+	#endregion
+
 	#region Fields: Private
 
 	private readonly Border _anchor;
@@ -35,6 +53,7 @@ public sealed class TrayPopupWindow : Window
 		Topmost = true;
 		Width = 1;
 
+		_contextMenu.Opened += OnContextMenuOpened;
 		Opened += OnOpened;
 	}
 
@@ -42,10 +61,20 @@ public sealed class TrayPopupWindow : Window
 
 	#region Methods: Private
 
+	[DllImport("user32.dll", SetLastError = true)]
+	[return: MarshalAs(UnmanagedType.Bool)]
+	private static extern bool GetCursorPos(out NativePoint point);
+
 	private void OnContextMenuClosed(object? sender, RoutedEventArgs eventArgs)
 	{
+		_contextMenu.Opened -= OnContextMenuOpened;
 		_contextMenu.Closed -= OnContextMenuClosed;
 		Close();
+	}
+
+	private void OnContextMenuOpened(object? sender, EventArgs eventArgs)
+	{
+		Dispatcher.UIThread.Post(SynchronizeSelectionWithCursor, DispatcherPriority.Input);
 	}
 
 	private void OnOpened(object? sender, EventArgs eventArgs)
@@ -54,6 +83,32 @@ public sealed class TrayPopupWindow : Window
 		_contextMenu.Closed += OnContextMenuClosed;
 		_contextMenu.Placement = PlacementMode.BottomEdgeAlignedLeft;
 		_contextMenu.Open(_anchor);
+	}
+
+	private void SynchronizeSelectionWithCursor()
+	{
+		if (!OperatingSystem.IsWindows()
+			|| !GetCursorPos(out var cursorPosition))
+		{
+			return;
+		}
+
+		var topLevel = TopLevel.GetTopLevel(_contextMenu);
+		if (topLevel is null)
+		{
+			return;
+		}
+
+		var cursorClientPoint = topLevel.PointToClient(new PixelPoint(cursorPosition.X, cursorPosition.Y));
+		var visual = topLevel.GetVisualAt(cursorClientPoint);
+		var menuItem = visual as MenuItem ?? visual?.FindAncestorOfType<MenuItem>();
+		if (menuItem is null || !menuItem.IsEffectivelyEnabled)
+		{
+			return;
+		}
+
+		menuItem.IsSelected = true;
+		menuItem.Focus();
 	}
 
 	#endregion
